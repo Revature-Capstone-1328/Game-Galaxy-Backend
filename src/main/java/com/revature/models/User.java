5d03b010b1eabc153e1b74743bc6d34ef1784ffe
Andres <joseanpeba@gmail.com>
2022-06-14 12:54:44 +0200

    updated models

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,8 +1,15 @@
 package com.revature.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 
 }
