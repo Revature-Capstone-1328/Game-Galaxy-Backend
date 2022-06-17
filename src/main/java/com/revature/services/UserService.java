5c3cdf1219d395508b6b2aeac614fccffa38e40a
SilvanGrosskreutz <silvan.grosskreutz@htp-tel.de>
2022-06-17 11:48:48 +0200

    Register + Login + Update

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,49 @@
 package com.revature.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.revature.models.User;
+import com.revature.repositories.UserDAO;
 
 @Service
 public class UserService {
-
+	
+	private UserDAO userDAO;
+	
+	@Autowired
+	public UserService(UserDAO userDAO) {
+		super();
+		this.userDAO = userDAO;
+	}
+	
+	public boolean register(User user) {
+		if(user!=null) {
+			user.setId(0);
+			userDAO.save(user);
+			return true;
+		}
+		return false;
+	}
+	
+	public User login(User user) {
+		Optional<User> dbUser = userDAO.getUserbyUsername(user.getUsername());
+		if(dbUser.isPresent()) {
+			if(dbUser.get().getPassword().equals(user.getPassword())) {
+				return dbUser.get();
+			}
+		}
+		return null;
+	}
+	
+	public User update(User user) {
+		Optional<User> dbUser = userDAO.getUserbyUsername(user.getUsername());
+		if(dbUser.isPresent()) {
+			userDAO.save(user);
+			return user;
+		}
+		return null;
+	}
 }
