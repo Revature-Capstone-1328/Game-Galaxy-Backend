dee12f4c3145a829dee229698b42cd7a7db3e612
unknown <kkaszka97@wp.pl>
2022-06-27 11:23:05 +0200

    wishlist finished

--- src/main/java/com/revature/models/WishList.java
+++ src/main/java/com/revature/models/WishList.java
@@ -7,8 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "userId", "gameID" }) })
 public class WishList implements Serializable
 {
 	@Id
